5c8f230cbf32579285a6392f78d6bdc333518b8a
Oriolac <oriolalascercos@gmail.com>
2019-12-09 19:35:09 +0100

    Fet bé el Pollards Lambda ¿?????????

--- src/cat/udl/cig/operations/wrapper/PollardsLambda.java
+++ src/cat/udl/cig/operations/wrapper/PollardsLambda.java
@@ -1,54 +1,88 @@
 package cat.udl.cig.operations.wrapper;
 
+import cat.udl.cig.exceptions.ConstructionException;
 import cat.udl.cig.exceptions.NotSolutionException;
 import cat.udl.cig.exceptions.ParametersException;
 import cat.udl.cig.fields.Group;
 import cat.udl.cig.fields.GroupElement;
+import javafx.util.Pair;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
+import java.security.SecureRandom;
+import java.util.HashMap;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
 public class PollardsLambda {
 
+    private final GroupElement alpha, beta;
+    private final Group group;
+    private final BigInteger b;
+    private final BigInteger N;
+    private HashMap<BigInteger, Pair<BigInteger, GroupElement>> hashMap;
 
-    private final static int N = 11;
 
-    public static BigInteger algorithm(GroupElement alpha, GroupElement beta) throws NotSolutionException {
-        //TODO: Preguntar a @miret o @fsebe per a tot Zn o per  a tot Zp 
+    public PollardsLambda(GroupElement alpha, GroupElement beta) {
         if (!alpha.belongsToSameGroup(beta)) {
-            throw new ParametersException();
+            throw new ConstructionException();
         }
+        group = alpha.getGroup();
+        b = group.getSize().subtract(BigInteger.ONE);
+        N = b.sqrt().add(BigInteger.ONE);
+        this.alpha = alpha;
+        this.beta = beta;
+    }
+
+
+    public Optional<BigInteger> algorithm() throws NotSolutionException {
+        //TODO: Preguntar a @miret o @fsebe per a tot Zn o per  a tot Zp. A vegades no funciona ni per Zp
+        Optional<BigInteger> res = Optional.empty();
+        int i = 0;
+        while (i < 10 && res.isEmpty()) {
+            getHashMap();
+            Pair<GroupElement, BigInteger> pair = getD();
+            res = getExponent(pair);
+            i++;
+        }
+        return res;
+    }
+
+    private void getHashMap() {
+        this.hashMap = new HashMap<>();
+        for (int k = 0; k < N.sqrt().intValue(); k++) {
+            BigInteger r = new BigInteger(group.getSize().bitLength(), new SecureRandom());
+            hashMap.put(BigInteger.valueOf(k), new Pair<>(r, alpha.pow(r)));
+        }
+    }
+
+    private Optional<BigInteger> getExponent(Pair<GroupElement, BigInteger> pair) {
         Group group = alpha.getGroup();
-        BigInteger b = group.getSize().subtract(BigInteger.ONE);
-        GroupElement xn = alpha.pow(b);
-        BigInteger d = BigInteger.ZERO;
-        for (int i = 0; i < N; i++) {
-            d = d.add(randomMap(xn));
-            xn = xn.multiply(alpha.pow(randomMap(xn)));
-        }
-        assertEquals(xn, alpha.pow(b.add(d)));
+        BigInteger d = pair.getValue();
+        GroupElement xn = pair.getKey();
         GroupElement yn = beta;
         BigInteger acc = BigInteger.ZERO;
-        while (!stopComputing(yn, xn, acc, b.add(d))) {
-            acc = acc.add(randomMap(yn));
-            yn = yn.multiply(alpha.pow(randomMap(yn)));
-            assertEquals(beta.multiply(alpha.pow(acc)), yn);
+        while (acc.compareTo(b.add(d)) > 0) {
+            Pair<BigInteger, GroupElement> tmp = hashMap.get(BigInteger.valueOf(yn.hashCode() % hashMap.size()));
+            acc = acc.add(tmp.getKey());
+            yn = yn.multiply(tmp.getValue());
+            if (xn.equals(yn)) {
+                return Optional.of(b.add(d).subtract(acc).remainder(group.getSize().subtract(BigInteger.ONE)));
+            }
+            //assertEquals(beta.multiply(alpha.pow(acc)), yn);
         }
-        return b.add(d).subtract(acc).remainder(group.getSize().subtract(BigInteger.ONE));
+        return Optional.empty();
     }
 
-    private static boolean stopComputing(GroupElement yn, GroupElement xn, BigInteger di, BigInteger bad) throws NotSolutionException {
-        if (di.compareTo(bad) > 0)
-            throw new NotSolutionException();
-        return xn.equals(yn);
+    private Pair<GroupElement, BigInteger> getD() {
+        GroupElement xn = alpha.pow(b);
+        BigInteger d = BigInteger.ZERO;
+        for (long i = 0; i < N.intValue(); i++) {
+            Pair<BigInteger, GroupElement> tmp = hashMap.get(BigInteger.valueOf(xn.hashCode() % hashMap.size()));
+            d = d.add(tmp.getKey());
+            xn = xn.multiply(tmp.getValue());
+        }
+        return new Pair<>(xn, d);
     }
 
-    private static BigInteger randomMap(GroupElement elem) {
-        //BigInteger module = elem.getGroup().getSize();
-        return elem.getIntValue().add(BigInteger.TWO);
-        //TODO: @vmateu change so it is random.
-    }
 }
--- test/cat/udl/cig/operations/wrapper/PollardsLambdaTest.java
+++ test/cat/udl/cig/operations/wrapper/PollardsLambdaTest.java
@@ -1,23 +1,52 @@
 package cat.udl.cig.operations.wrapper;
 
-import cat.udl.cig.fields.Group;
-import cat.udl.cig.fields.GroupElement;
-import cat.udl.cig.fields.IntegerPrimeOrderSubgroup;
+import cat.udl.cig.ecc.ECPrimeOrderSubgroup;
+import cat.udl.cig.ecc.GeneralEC;
+import cat.udl.cig.ecc.GeneralECPoint;
+import cat.udl.cig.fields.*;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PollardsLambdaTest {
+/*
+    static BigInteger MODULE = new BigInteger("7");
+    static BigInteger n = new BigInteger("5");
+    static BigInteger b = new BigInteger("1", 16);
+    static BigInteger gx = new BigInteger("0", 16);
+    static BigInteger gy = new BigInteger("6"
+            .replaceAll("\\s", ""), 16);
+    static RingElement[] COEF = new RingElement[2];
+    static ArrayList<BigInteger> card = new ArrayList<>();
+    static GeneralECPoint gen;
 
     @Test
     void algorithm() {
         Group g = new IntegerPrimeOrderSubgroup(BigInteger.valueOf(11), BigInteger.valueOf(10), BigInteger.valueOf(7));
         GroupElement alpha = g.toElement(BigInteger.valueOf(7));
-        BigInteger x = g.getRandomExponent();
+        BigInteger x = BigInteger.valueOf(4);
         GroupElement beta = alpha.pow(x);
         System.out.println("Alpha: " + alpha.getIntValue().toString() + "   x: " + x.toString() + "    beta: " + beta.getIntValue().toString());
         assertEquals(x, PollardsLambda.algorithm(alpha, beta));
     }
+
+    @Test
+    void algorithm_ecc() {
+        PrimeField ring = new PrimeField(MODULE);
+
+        COEF[0] = new PrimeFieldElement(ring, BigInteger.valueOf(1));
+        COEF[1] = new PrimeFieldElement(ring, b);
+        card.add(n);
+        GeneralEC curve = new GeneralEC(ring, COEF, card);
+        gen = new GeneralECPoint(curve, new PrimeFieldElement(ring, gx), new PrimeFieldElement(ring, gy));
+        ECPrimeOrderSubgroup g = new ECPrimeOrderSubgroup(curve, n, gen);
+        GeneralECPoint alpha = g.getGenerator();
+        BigInteger x = g.getRandomExponent();
+        GeneralECPoint beta = alpha.pow(x);
+        System.out.println("Alpha: " + alpha.toString() + "   x: " + x.toString() + "    beta: " + beta.toString());
+        assertEquals(x, PollardsLambda.algorithm(alpha, beta));
+    }*/
 }
