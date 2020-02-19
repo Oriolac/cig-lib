771b6915681bac1d761923414891fa2b0e67df2c
Oriol <oriolalascercos@gmail.com>
2020-02-19 15:39:03 +0100

    Used the order of the biginteger

--- src/cat/udl/cig/operations/wrapper/BruteForce.java
+++ src/cat/udl/cig/operations/wrapper/BruteForce.java
@@ -8,14 +8,21 @@ import java.util.Optional;
 
 public class BruteForce implements LogarithmAlgorithm {
     GroupElement alpha;
+    BigInteger last;
 
     public BruteForce(GroupElement alpha) {
         this.alpha = alpha;
+        last = alpha.getGroup().getSize();
+    }
+
+    public BruteForce(GroupElement alpha, BigInteger last) {
+        this.alpha = alpha;
+        this.last = last;
     }
 
     @Override
     public Optional<BigInteger> algorithm(GroupElement beta) throws ArithmeticException {
-        for (int i = 0; BigInteger.valueOf(i).compareTo(beta.getGroup().getSize()) < 0; i++) {
+        for (int i = 0; BigInteger.valueOf(i).compareTo(last) < 0; i++) {
             BigInteger x = BigInteger.valueOf(i);
             if (alpha.pow(x).equals(beta))
                 return Optional.of(x);
