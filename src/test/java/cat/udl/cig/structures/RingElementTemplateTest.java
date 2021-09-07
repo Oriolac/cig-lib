a81ef8b589e957ff8ec03fdaabbb7f8838c66f68
Marta Albets Mitjaneta <marta.albets@gmail.com>
2021-09-07 13:07:14 +0200

    More tests done

--- src/test/java/cat/udl/cig/structures/RingElementTemplateTest.java
+++ src/test/java/cat/udl/cig/structures/RingElementTemplateTest.java
@@ -3,12 +3,16 @@ package cat.udl.cig.structures;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public abstract class RingElementTemplateTest {
 
     private Ring ring;
     private RingElement op1;
     private RingElement op2;
     private RingElement resultAddition;
+    private RingElement expectedMultiplication;
+    private RingElement expectedDivision;
 
     @BeforeEach
     void setUp() {
@@ -16,6 +20,8 @@ public abstract class RingElementTemplateTest {
         op1 = returnOperand1();
         op2 = returnOperand2();
         resultAddition = returnResultAddition();
+        expectedMultiplication = expectedResultMultiplication();
+        expectedDivision = expectedResultDivision();
     }
 
     protected abstract Ring returnRing();
@@ -25,6 +31,10 @@ public abstract class RingElementTemplateTest {
     protected abstract RingElement returnOperand2();
 
     protected abstract RingElement returnResultAddition();
+
+    protected abstract RingElement expectedResultMultiplication();
+
+    protected abstract RingElement expectedResultDivision();
 
     @Test
     void testAdditionWithIdentity() {
@@ -43,17 +53,28 @@ public abstract class RingElementTemplateTest {
 
     @Test
     void testMultiplicationWithIdentity() {
+        RingElement result = op1.multiply(ring.getMultiplicativeIdentity());
+        assertEquals(op1, result);
 
     }
 
     @Test
     void testMultiplicationTwoOps() {
+        RingElement result = op1.multiply(op2);
+        assertEquals(expectedMultiplication, result);
 
     }
 
     @Test
-    void testDivisionTwoOps() {
+    void testDivisionWithIdentity() {
+        RingElement result = op1.divide(ring.getMultiplicativeIdentity());
+        assertEquals(op1, result);
+    }
 
+    @Test
+    void testDivisionTwoOps() {
+        RingElement result = op1.divide(op2);
+        assertEquals(expectedDivision, result);
     }
 
     @Test
