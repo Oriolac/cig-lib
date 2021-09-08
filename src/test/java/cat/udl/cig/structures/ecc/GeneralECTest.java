654c98f18ed93551ccb759afeccd6ec106170968
Oriol Alàs <oriolalascercos@gmail.com>
2021-09-08 13:29:24 +0200

    GeneralECTest [6:]

--- src/test/java/cat/udl/cig/structures/ecc/GeneralECTest.java
+++ src/test/java/cat/udl/cig/structures/ecc/GeneralECTest.java
@@ -6,16 +6,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 abstract class GeneralECTest {
 
-    GeneralEC generalEC;
-    GeneralECPoint point1;
-    GeneralECPoint point2;
-    RingElement xCoordinateFromPoint1;
-    RingElement yCoordinateFromPoint1;
-    Ring ring;
+    private GeneralEC generalEC;
+    private GeneralECPoint point1;
+    private GeneralECPoint point2;
+    private RingElement xCoordinateFromPoint1;
+    private RingElement yCoordinateFromPoint1;
+    private BigInteger expectedOrderPoint1;
+    private Ring ring;
+    private RingElement xBadCoordinate;
 
     @BeforeEach
     void setUp() {
@@ -23,17 +27,23 @@ abstract class GeneralECTest {
         ring = returnRingOfEC();
         point1 = returnGeneralECPoint1();
         point2 = returnGeneralECPoint2();
+        xBadCoordinate = returnBadCoordinateX();
         xCoordinateFromPoint1 = point1.x;
         yCoordinateFromPoint1 = point1.y;
+        expectedOrderPoint1 = returnExpectedOrderOfPoint1();
     }
 
     protected abstract GeneralEC returnGeneralEC();
 
     protected abstract Ring returnRingOfEC();
 
+    protected abstract RingElement returnBadCoordinateX();
+
     protected abstract GeneralECPoint returnGeneralECPoint1();
 
     protected abstract GeneralECPoint returnGeneralECPoint2();
+
+    protected abstract BigInteger returnExpectedOrderOfPoint1();
 
     @Test
     void testElementPlusInfinityEqualsElement() {
@@ -61,34 +71,58 @@ abstract class GeneralECTest {
     }
 
     @Test
-    void testIsCoordenatesOnCurve() {
+    void testAreCoordinatesOnCurve() {
 
     }
 
     @Test
     void testCorrectLiftX() {
+        testCorrectLiftX(point1);
+        testCorrectLiftX(point2);
+    }
 
+    private void testCorrectLiftX(GeneralECPoint point1) {
+        Optional<? extends GeneralECPoint> point = generalEC.liftX(point1.x);
+        assertTrue(point.isPresent());
+        assertEquals(point.get(), point1);
+        assertTrue(ring.containsElement(point.get().x));
+        assertTrue(ring.containsElement(point.get().y));
     }
 
     @Test
     void testIncorrectLiftX() {
-
+        Optional<? extends GeneralECPoint> point = generalEC.liftX(xBadCoordinate);
+        assertTrue(point.isEmpty());
     }
 
     @Test
     void testIsOnCurveAndContainsElement() {
+        testIsOnCurveAndContainsElement(point1);
+        testIsOnCurveAndContainsElement(point2);
+    }
 
+    private void testIsOnCurveAndContainsElement(GeneralECPoint point1) {
+        generalEC.isOnCurve(point1);
+        generalEC.containsElement(point1);
+        GeneralECPoint point = new GeneralEC(generalEC).liftX(point1.x).orElseThrow();
+        assertEquals(point1, point);
+        assertTrue(generalEC.containsElement(point1));
+        assertTrue(generalEC.containsElement(point));
     }
 
     @Test
     void testComputesOrder() {
-
+        BigInteger orderPoint1 = generalEC.computeOrder(point1);
+        assertNotNull(orderPoint1);
+        assertEquals(this.expectedOrderPoint1, orderPoint1);
     }
 
     @Test
     void testAreCoordinatesOfAPointFromSameField() {
-
+        Optional<? extends GeneralECPoint> point = generalEC.liftX(point1.x);
+        assertTrue(point.isPresent());
+        assertTrue(ring.containsElement(point.get().x));
+        assertTrue(ring.containsElement(point.get().y));
+        assertTrue(point.get().x.belongsToSameGroup(point.get().y));
     }
-
-
 }
