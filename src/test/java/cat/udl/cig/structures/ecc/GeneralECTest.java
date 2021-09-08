5e3f92be0a4a972d8d982d2662dfeb08529b7a27
Marta Albets Mitjaneta <marta.albets@gmail.com>
2021-09-08 13:44:30 +0200

    More tests started

--- src/test/java/cat/udl/cig/structures/ecc/GeneralECTest.java
+++ src/test/java/cat/udl/cig/structures/ecc/GeneralECTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.math.BigInteger;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 abstract class GeneralECTest {
 
     GeneralEC generalEC;
@@ -15,6 +17,8 @@ abstract class GeneralECTest {
     GeneralECPoint point2;
     RingElement xCoordinateFromPoint1;
     RingElement yCoordinateFromPoint1;
+    RingElement xCoordinateFromPoint2;
+    RingElement yCoordinateFromPoint2;
     Ring ring;
 
     @BeforeEach
@@ -25,6 +29,8 @@ abstract class GeneralECTest {
         point2 = returnGeneralECPoint2();
         xCoordinateFromPoint1 = point1.x;
         yCoordinateFromPoint1 = point1.y;
+        xCoordinateFromPoint2 = point2.x;
+        yCoordinateFromPoint2 = point2.y;
     }
 
     protected abstract GeneralEC returnGeneralEC();
@@ -37,32 +43,53 @@ abstract class GeneralECTest {
 
     @Test
     void testElementPlusInfinityEqualsElement() {
-
+        GeneralECPoint inf = generalEC.getMultiplicativeIdentity();
+        GeneralECPoint elem1 = point1.multiply(inf);
+        GeneralECPoint elem2 = point2.multiply(inf);
+        assertEquals(point1, elem1);
+        assertEquals(point2, elem2);
     }
 
     @Test
     void testElementMult1EqualsElement() {
+        BigInteger elem = (BigInteger.ONE);
+        GeneralECPoint mult1 = point1.pow(elem);
+        GeneralECPoint mult2 = point2.pow(elem);
+        assertEquals(point1, mult1);
+        assertEquals(point2, mult2);
 
     }
 
     @Test
     void testElementPlusAnotherElement() {
+        GeneralECPoint plus = generalEC.multiply(point1, point2);
+        assertEquals(point1.multiply(point2), plus);
 
     }
 
     @Test
     void testElementMultByNonZeroScalar() {
-
+        BigInteger power = BigInteger.valueOf(3L);
+        GeneralECPoint mult = generalEC.pow(point1, power);
+        assertEquals(point1.pow(power), mult);
     }
 
     @Test
     void testIsElementOnCurve() {
-
+        boolean elem1 = generalEC.isOnCurve(point1);
+        boolean elem2 = generalEC.isOnCurve(point2);
+        assertEquals(point1.getCurve().equals(generalEC), elem1);
+        assertEquals(point2.getCurve().equals(generalEC), elem2);
     }
 
     @Test
     void testIsCoordenatesOnCurve() {
-
+        boolean elem1 = generalEC.isOnCurve(xCoordinateFromPoint1, yCoordinateFromPoint1);
+        boolean elem2 = generalEC.isOnCurve(xCoordinateFromPoint2, yCoordinateFromPoint2);
+        boolean cond1 = xCoordinateFromPoint1.getGroup().equals(ring) && !xCoordinateFromPoint1.getGroup().equals(yCoordinateFromPoint1.getGroup());
+        boolean cond2 = xCoordinateFromPoint2.getGroup().equals(ring) && !xCoordinateFromPoint2.getGroup().equals(yCoordinateFromPoint2.getGroup());
+        assertEquals(cond1, elem1);
+        assertEquals(cond2, elem2);
     }
 
     @Test
