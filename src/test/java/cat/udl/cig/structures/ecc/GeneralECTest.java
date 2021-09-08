f8ed88c6dce39a7466bd1c070718f221e2561840
Oriol Alàs <oriolalascercos@gmail.com>
2021-09-08 13:05:33 +0200

    generalEcTest

--- src/test/java/cat/udl/cig/structures/ecc/GeneralECTest.java
+++ src/test/java/cat/udl/cig/structures/ecc/GeneralECTest.java
@@ -3,6 +3,7 @@ package cat.udl.cig.structures.ecc;
 import cat.udl.cig.structures.Ring;
 import cat.udl.cig.structures.RingElement;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -10,15 +11,81 @@ import java.util.ArrayList;
 abstract class GeneralECTest {
 
     GeneralEC generalEC;
+    GeneralECPoint point1;
+    GeneralECPoint point2;
     Ring ring;
 
     @BeforeEach
     void setUp() {
         generalEC = returnGeneralEC();
+        ring = returnRingOfEC();
+        point1 = returnGeneralECPoint1();
+        point2 = returnGeneralECPoint2();
+
     }
 
     protected abstract GeneralEC returnGeneralEC();
 
+    protected abstract Ring returnRingOfEC();
+
+    protected abstract GeneralECPoint returnGeneralECPoint1();
+
+    protected abstract GeneralECPoint returnGeneralECPoint2();
+
+    @Test
+    void testElementPlusInfinityEqualsElement() {
+
+    }
+
+    @Test
+    void testElementMult1EqualsElement() {
+
+    }
+
+    @Test
+    void testElementPlusAnotherElement() {
+
+    }
+
+    @Test
+    void testElementMultByNonZeroScalar() {
+
+    }
+
+    @Test
+    void testIsElementOnCurve() {
+
+    }
+
+    @Test
+    void testIsCoordenatesOnCurve() {
+
+    }
+
+    @Test
+    void testCorrectLiftX() {
+
+    }
+
+    @Test
+    void testIncorrectLiftX() {
+
+    }
+
+    @Test
+    void testIsOnCurveAndContainsElement() {
+
+    }
+
+    @Test
+    void testComputesOrder() {
+
+    }
+
+    @Test
+    void testAreCoordinatesOfAPointFromSameField() {
+
+    }
 
 
 }
