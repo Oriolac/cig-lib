cd2591e88dfb4c747a0a6f9b561b185d39c461c6
Marta Albets Mitjaneta <marta.albets@gmail.com>
2021-09-09 10:52:29 +0200

    More tests

--- src/test/java/cat/udl/cig/structures/ecc/PrimeFieldECTest.java
+++ src/test/java/cat/udl/cig/structures/ecc/PrimeFieldECTest.java
@@ -1,6 +1,7 @@
 package cat.udl.cig.structures.ecc;
 
 import cat.udl.cig.structures.PrimeField;
+import cat.udl.cig.structures.PrimeFieldElement;
 import cat.udl.cig.structures.Ring;
 import cat.udl.cig.structures.RingElement;
 import cat.udl.cig.structures.builder.PrimeFieldElementBuilder;
@@ -50,12 +51,20 @@ public class PrimeFieldECTest extends GeneralECTest {
 
     @Override
     protected GeneralECPoint returnExpectedResultPlusOperation() {
-        return null;
+        String point1Str = "994";
+        BigInteger x = new BigInteger(point1Str);
+        String point2Str = "1928";
+        BigInteger y = new BigInteger(point2Str);
+        return new GeneralECPoint(curve, new PrimeFieldElement(primeField, x), new PrimeFieldElement(primeField, y));
     }
 
     @Override
     protected GeneralECPoint returnsExpectedElementMultByNonZeroScalar() {
-        return null;
+        String point1Str = "297";
+        BigInteger x = new BigInteger(point1Str);
+        String point2Str = "968";
+        BigInteger y = new BigInteger(point2Str);
+        return new GeneralECPoint(curve, new PrimeFieldElement(primeField, x), new PrimeFieldElement(primeField, y));
     }
 
     @Override
