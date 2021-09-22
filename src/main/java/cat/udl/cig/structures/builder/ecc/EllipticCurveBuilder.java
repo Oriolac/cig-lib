089ab60a212b47035144965c3355155a8d491eb5
Marta Albets Mitjaneta <marta.albets@gmail.com>
2021-09-22 11:52:51 +0200

    EllipticCurveBuilder modified

--- src/main/java/cat/udl/cig/structures/builder/ecc/EllipticCurveBuilder.java
+++ src/main/java/cat/udl/cig/structures/builder/ecc/EllipticCurveBuilder.java
@@ -11,11 +11,21 @@ import java.math.BigInteger;
 public class EllipticCurveBuilder {
 
     private final Ring ring;
-    private final RingElement a1;
+    private RingElement a1;
+    private RingElement a2;
+
+    private final String A1 = "A1";
 
     public EllipticCurveBuilder() {
         ring = null;
         a1 = null;
+        a2 = null;
+    }
+
+
+    public EllipticCurveBuilder(EllipticCurveBuilder builder) {
+        this.ring = builder.ring;
+        this.a1 = builder.a1;
     }
 
     public EllipticCurveBuilder(EllipticCurveBuilder builder, Ring ring) {
@@ -23,10 +33,6 @@ public class EllipticCurveBuilder {
         this.a1 = builder.a1;
     }
 
-    public EllipticCurveBuilder(EllipticCurveBuilder builder, RingElement a1) {
-        this.ring = builder.ring;
-        this.a1 = a1;
-    }
 
     public EllipticCurveBuilder setRing(Ring ring) {
         if (this.ring == null)
@@ -38,11 +44,22 @@ public class EllipticCurveBuilder {
     }
 
     public EllipticCurveBuilder setA1(RingElement a1) {
-        if (this.a1 == null)
-            return new EllipticCurveBuilder(this, a1);
+        if (this.a1 == null) {
+            EllipticCurveBuilder res = new EllipticCurveBuilder(this);
+            res.a1 = a1;
+            return res;
+        }
         if (this.a1.equals(a1))
             return this;
         throw new IllegalStateException("RingElement a1 already set in that builder");
+    }
+
+    public EllipticCurveBuilder setA2(RingElement a2) {
+        if (this.a2 == null)
+            return new EllipticCurveBuilder(this, a2);
+        if (this.a2.equals(a2))
+            return this;
+        throw new IllegalStateException("RingElement a2 already set in that builder");
     }
 
 
@@ -57,7 +74,6 @@ public class EllipticCurveBuilder {
     static public Pair<EllipticCurve, GeneralECPoint> reducedFormOnlyOneSubgroup(Ring ring, RingElement A, RingElement B, BigInteger order, RingElement x, RingElement y) {
         return EllipticCurve.EllipticCurveGeneratorOnlyOneSubgroup(ring, A, B, order, x, y);
     }
-
 
 
 }
