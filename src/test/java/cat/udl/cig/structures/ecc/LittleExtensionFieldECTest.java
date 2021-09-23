449d15f4af130aa422ebd537a31d4721ceb09bac
Oriol Alàs <oriolalascercos@gmail.com>
2021-09-23 12:27:53 +0200

    merge

--- src/test/java/cat/udl/cig/structures/ecc/LittleExtensionFieldECTest.java
+++ src/test/java/cat/udl/cig/structures/ecc/LittleExtensionFieldECTest.java
@@ -3,15 +3,15 @@ package cat.udl.cig.structures.ecc;
 import cat.udl.cig.structures.ExtensionField;
 import cat.udl.cig.structures.ExtensionFieldElement;
 import cat.udl.cig.structures.PrimeField;
+import cat.udl.cig.structures.RingElement;
 import cat.udl.cig.utils.Polynomial;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LittleExtensionFieldECTest {
 
@@ -57,19 +57,30 @@ public class LittleExtensionFieldECTest {
                                 .addTerm(1, primeField.buildElement().setValue(2).build().orElseThrow())
                                 .addTerm(0, primeField.buildElement().setValue(2).build().orElseThrow()).build()).build().orElseThrow()
                 )));
-
-
-
-
-
-
-
-
-
-
-
-
-
+        points.put(field.buildElement().setPolynomial(new Polynomial.PolynomialBuilder()
+                        .addTerm(1, primeField.getMultiplicativeIdentity())
+                        .addTerm(0, primeField.buildElement().setValue(2).build().orElseThrow())
+                        .build()).build().orElseThrow(),
+                new HashSet<>(List.of(
+                        field.buildElement().setPolynomial(new Polynomial.PolynomialBuilder()
+                                .addTerm(0, primeField.getMultiplicativeIdentity())
+                                .build()).build().orElseThrow(),
+                        field.buildElement().setPolynomial(new Polynomial.PolynomialBuilder()
+                                .addTerm(0, primeField.buildElement().setValue(2).build().orElseThrow())
+                                .build()).build().orElseThrow()
+                )));
+        points.put(field.buildElement().setPolynomial(new Polynomial.PolynomialBuilder()
+                        .addTerm(1, primeField.buildElement().setValue(2).build().orElseThrow())
+                        .addTerm(0, primeField.buildElement().setValue(2).build().orElseThrow())
+                        .build()).build().orElseThrow(),
+                new HashSet<>(List.of(
+                        field.buildElement().setPolynomial(new Polynomial.PolynomialBuilder()
+                                .addTerm(1, primeField.buildElement().setValue(1).build().orElseThrow())
+                                .build()).build().orElseThrow(),
+                        field.buildElement().setPolynomial(new Polynomial.PolynomialBuilder()
+                                .addTerm(1, primeField.buildElement().setValue(2).build().orElseThrow())
+                                .build()).build().orElseThrow()
+                )));
 
         points.put(field.buildElement().setPolynomial(new Polynomial.PolynomialBuilder().addTerm(1, primeField.buildElement().setValue(2).build().orElseThrow()).build()).build().orElseThrow(),
                 new HashSet<>(List.of(
@@ -82,6 +93,16 @@ public class LittleExtensionFieldECTest {
                                 .addTerm(0, primeField.getMultiplicativeIdentity())
                                 .build()).build().orElseThrow()
                 )));
+
+        points.forEach((a, b) -> {
+            ArrayList<? extends GeneralECPoint> actualPoints = ec.liftX(a);
+            for (GeneralECPoint actualPoint : actualPoints) {
+                RingElement y = actualPoint.getY();
+                assertTrue(y instanceof ExtensionFieldElement);
+                assertTrue(b.contains((ExtensionFieldElement) actualPoint.getY()));
+            }
+        });
+
     }
 }
 
